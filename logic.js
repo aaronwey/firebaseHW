95bcc8b7f386f60c8c8c4d300a68ee81d05f8e6d
aaronwey <aaronwey@gmail.com>
2017-09-06 12:52:40 -0600

    3rd commit

--- logic.js
+++ logic.js
@@ -1,32 +1,32 @@
 var config = {
-    apiKey: "AIzaSyBINCOQ8E6IpbQI9-8HKBDvZF_MCFk7Sfs",
-    authDomain: "weddemo-1afa1.firebaseapp.com",
-    databaseURL: "https://weddemo-1afa1.firebaseio.com",
-    projectId: "weddemo-1afa1",
-    storageBucket: "weddemo-1afa1.appspot.com",
-    messagingSenderId: "683297747668"
-  };
-  firebase.initializeApp(config);
+  apiKey: "AIzaSyBINCOQ8E6IpbQI9-8HKBDvZF_MCFk7Sfs",
+  authDomain: "weddemo-1afa1.firebaseapp.com",
+  databaseURL: "https://weddemo-1afa1.firebaseio.com",
+  projectId: "weddemo-1afa1",
+  storageBucket: "weddemo-1afa1.appspot.com",
+  messagingSenderId: "683297747668"
+};
+firebase.initializeApp(config);
 
-  var database = firebase.database();
+var database = firebase.database();
 
-  var TrainName = "";
-  var Destination = "";
-  var FirstTrainTime = "";
-  var tFrequency = "";
-  var minutesAway = "";
-  var nextArrival = "";
+var TrainName = "";
+var Destination = "";
+var FirstTrainTime = "";
+var tFrequency = "";
+var minutesAway = "";
+var nextArrival = "";
 
 
 
 
 $(".submitButton").on("click", function(){
-	event.preventDefault();
+  event.preventDefault();
 
-	TrainName = $(".TrainName").val().trim();
-	Destination = $(".Destination").val().trim();
-	FirstTrainTime = $(".FirstTrainTime").val().trim();
-	tFrequency = $(".tFrequency").val().trim();
+  TrainName = $(".TrainName").val().trim();
+  Destination = $(".Destination").val().trim();
+  FirstTrainTime = $(".FirstTrainTime").val().trim();
+  tFrequency = $(".tFrequency").val().trim();
 
   var newTrain = {
     TrainName: TrainName,
@@ -44,13 +44,13 @@ $(".submitButton").on("click", function(){
 
 });
 
-  database.ref().on("child_added", function(childSnapshot, prevChildKey){
+database.ref().on("child_added", function(childSnapshot, prevChildKey){
 
-    TrainName = childSnapshot.val().TrainName;
-    Destination = childSnapshot.val().Destination;
-    FirstTrainTime = childSnapshot.val().FirstTrainTime;
-    tFrequency = childSnapshot.val().tFrequency;
-    
+  TrainName = childSnapshot.val().TrainName;
+  Destination = childSnapshot.val().Destination;
+  FirstTrainTime = childSnapshot.val().FirstTrainTime;
+  tFrequency = childSnapshot.val().tFrequency;
+
   FirstTrainConverted = moment(FirstTrainTime, "hh:mm").subtract(1, "years");
   console.log(FirstTrainConverted);
   var currentTime = moment();
@@ -67,18 +67,14 @@ $(".submitButton").on("click", function(){
   console.log(tMinutesTillTrain);
   console.log(nextArrival);
 
+  var childTrainName = childSnapshot.val().TrainName;
+  var childDestination = childSnapshot.val().Destination;
+  var childTFrequency = childSnapshot.val().tFrequency;
+  
 
 
 
-    $(".tableBody").append("<tr><td>" + childSnapshot.val().TrainName +"</td><td>" + childSnapshot.val().Destination + "</td><td>" + childSnapshot.val().tFrequency + "</td><td>" + nextArrival + "</td><td>" + tMinutesTillTrain +"</td></tr>");
-        // $(".tableBody").append("<tr><td>" + childSnapshot.val().TrainName +"</td><td>" + childSnapshot.val().Destination + "</td><td>" + childSnapshot.val().tFrequency +"</td></tr>");
+  $(".tableBody").append("<tr><td>" + childTrainName +"</td><td>" + childDestination + "</td><td>" + childTFrequency + "</td><td>" + nextArrival + "</td><td>" + tMinutesTillTrain +"</td></tr>");
+
 });
 
-// $(".submitButton").on("click", function(){
-//   event.preventDefault();
-//   var newInfo = {
-//     minutesAway: minutesAway,
-//     nextArrival: nextArrival
-//   };
-//   database.ref().push(newInfo);
-// });
